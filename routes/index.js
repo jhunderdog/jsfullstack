703708b1dfc7ab03870c8db51b71aa23899c23eb
David <33382water@gmail.com>
2019-03-02 15:21:03 +0900

    login ejs

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ router.get('/', (req, res, next) => {
     res.render('welcome');
 });
 
-// router.get('/dashboard', (req, res, next) => {
-//     res.render('dashboard', {
-//         name: req.user.name
-//     })
-// })
+router.get('/dashboard', (req, res, next) => {
+    res.render('dashboard', {
+        name: req.user.name
+    })
+})
 module.exports = router;

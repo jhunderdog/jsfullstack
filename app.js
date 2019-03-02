67faa3346c9360027027aec327101b3c73cc8fbf
David <33382water@gmail.com>
2019-03-02 15:01:11 +0900

    ejs welcome

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const expressejsLayouts = require('express-ejs-layouts');
 const app = express();
+app.use(expressejsLayouts);
+app.set('view engine', 'ejs');
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('Welcome');
+    // res.send('Welcome');
+    res.render('welcome');
 });
 
 module.exports = router;

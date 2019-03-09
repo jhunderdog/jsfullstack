bc6c56e6574ac01358cf9ec5207d3fc40d79717f
David <33382water@gmail.com>
2019-03-09 14:15:12 +0900

    Passport

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const express = require('express');
 const expressejsLayouts = require('express-ejs-layouts');
-const app = express();
-const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const app = express();
+require('./config/passport')(passport);
+
+
 
 const db = require('./config/keys').mongoURI;
 
 mongoose.connect(db, { useNewUrlParser: true })
     .then( () => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
-    
-app.use(flash());
+
 app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
 }));
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//Connect Flash
+app.use(flash());
+
+//Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+}); 
+
 app.use(expressejsLayouts);
 app.set('view engine', 'ejs');
 //Express body parser
